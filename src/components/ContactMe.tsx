cbb7a47daad8b1b45661719a8dc103f798c1f87c
ycho-eth <130313736+ycho-eth@users.noreply.github.com>
2023-09-27 01:08:53 +0300

    commit

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -20,7 +20,7 @@ function ContactMe() {
             <h1 className="font-title relative text-6xl max-xl:text-4xl font-bold w-fit">Contact Me</h1>
             <div className="mt-20 flex gap-10 items-center justify-center max-md:flex-col">
                 <div className="flex flex-col gap-5 max-md:flex-row max-md:flex-wrap">
-                    <Card className="flex-1">
+                    <Card className="basis-1/3 grow">
                         <CardHeader>
                             <CardTitle className="flex justify-center gap-2 text-base items-center">
                                 <img className="h-[32px] w-[32px]" src={mail} alt=""/>
@@ -35,7 +35,7 @@ function ContactMe() {
                                                                        src={arrowright} alt=""/></a>
                         </CardFooter>
                     </Card>
-                    <Card className="flex-1">
+                    <Card className="basis-1/3 grow">
                         <CardHeader>
                             <CardTitle className="flex justify-center gap-2 text-base items-center">
                                 <img className="h-[32px] w-[32px]" src={linkedin} alt=""/>
@@ -50,7 +50,7 @@ function ContactMe() {
                                                                        src={arrowright} alt=""/></a>
                         </CardFooter>
                     </Card>
-                    <Card className="flex-1">
+                    <Card className="basis-1/3 grow">
                         <CardHeader>
                             <CardTitle className="flex justify-center gap-2 text-base items-center">
                                 <img src={twitter} alt=""/>
