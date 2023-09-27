2d39bdee14de02a927a49aa6666a1655c335b11d
ycho-eth <130313736+ycho-eth@users.noreply.github.com>
2023-09-27 11:59:00 +0300

    commit

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,6 +7,7 @@ function Card({title, from, image, skills}: ICertificates) {
 
     const handleMouseEnter = () => {
         setHovered(true);
+        console.log("Touch start")
     };
 
     const handleMouseLeave = () => {
@@ -15,7 +16,7 @@ function Card({title, from, image, skills}: ICertificates) {
 
     return (
         <div className={"flex flex-col border-white border-[1.5px] pb-5 rounded-lg item text-center justify-between transition-all"} onMouseEnter={handleMouseEnter}
-             onMouseLeave={handleMouseLeave}>
+             onMouseLeave={handleMouseLeave} onTouchStart={handleMouseEnter} onTouchEnd={handleMouseLeave}>
             <img className={`transition-all basis-8/12 object-cover ${(hovered) ? "grayscale-0" : "grayscale"}`} src={image} alt=""/>
             <h1 className="text-[#ff5d73] font-title text-xl max-lg:text-lg">{title}</h1>
             <h3 className="text-lg max-lg:text-base">{from}</h3>
